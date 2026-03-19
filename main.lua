fce77ecb52c345368726290fb222476921069205
Dawsig <isonrealyt@gmail.com>
2026-03-19 12:33:26 +0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,10 @@
+local Crescent = {
+    Places = {
+        [3351674303] = {
+            game = "Driving Empire",
+            place = "Main",
+            url = "https://example.com/Sea1.lua"
+        },
+    Universal = "nil"
+}
+return Crescent
