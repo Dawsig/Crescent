c5bb027a2353cf3c032c5e794990a005c0423c35
Dawsig <isonrealyt@gmail.com>
2026-03-19 12:37:14 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local Crescent = {
         [3351674303] = {
             game = "Driving Empire",
             place = "Main",
-            url = "https://example.com/Sea1.lua"
+            url = "https://raw.githubusercontent.com/Dawsig/Crescent/refs/heads/main/Games/Driving%20Empire"
         },
     Universal = "nil"
 }
