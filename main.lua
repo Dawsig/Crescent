7a464b5a2ac5dce08f27d1d4191d341cdf29eaa8
Dawsig <isonrealyt@gmail.com>
2026-03-30 15:45:32 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,6 +5,12 @@ local Crescent = {
             place = "Main",
             url = "https://raw.githubusercontent.com/Dawsig/Crescent/main/Games/Driving%20Empire.lua"
         }
+        [71008479572236] = {
+            game = "Bite By Night",
+            place = "Main",
+            url = "https://raw.githubusercontent.com/Dawsig/Crescent/main/Games/Bite%20By%20Night.lua"
+        }
+
     },
 
     Universal = nil
